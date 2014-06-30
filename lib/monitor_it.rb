baf11bb68a0989ff205219c36748547b76974f03
Zohaib Sarwar <zohaib.sarwar@confiz.com>
2014-06-30 11:59:56 +0500

    default value for sending and fetching data from server

--- lib/monitor_it.rb
+++ lib/monitor_it.rb
@@ -3,7 +3,7 @@ require 'library/save_data'
 
 module MonitorIt
   class Rack
-    def initialize(app, options = {:subscribe_me => {:options => [], :enabled_environments=> [], :host => "", :port => "", :create_path => "/request/create", :fetch_path => "/" }})
+    def initialize(app, options = {:subscribe_me => {:options => [], :enabled_environments=> [], :host => "", :port => "", :create_path => "", :fetch_path => "/" }})
       @app = app
       @@subscribe_to ||= (options[:subscribe_me][:options] and options[:subscribe_me][:enabled_environments].include?(Rails.env)) ? options[:subscribe_me][:options] : []
       @@subscribe_to.each do |opt|
@@ -12,7 +12,9 @@ module MonitorIt
         end
       end
 
-      if options[:subscribe_me][:host].present? and options[:subscribe_me][:port].present? and options[:subscribe_me][:create_path].present? and options[:subscribe_me][:fetch_path].present?
+      options[:subscribe_me][:create_path] = '/request/create' if options[:subscribe_me][:create_path].blank?
+      options[:subscribe_me][:fetch_path] = '/' if options[:subscribe_me][:fetch_path].blank?
+      if options[:subscribe_me][:host].present? and options[:subscribe_me][:port].present?
         @@host_port_create_path = options[:subscribe_me][:host] + ":" + options[:subscribe_me][:port] + options[:subscribe_me][:create_path]
         @@host_port_fetch_path = options[:subscribe_me][:host] + ":" + options[:subscribe_me][:port] + options[:subscribe_me][:fetch_path]
       else
